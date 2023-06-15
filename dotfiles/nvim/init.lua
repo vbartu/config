69a873ab7ea95aee0c354894b5b2ca61b1471cd5
Vicente Bartual <vicentebartualf@gmail.com>
2023-06-15 16:40:29 +0200

    Add lsp and autocompletion

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -63,9 +63,9 @@ set statusline+=\ %3p%%\ (%l/%L)\ %4l:%-3c
 -- F2: Set 80 char limit
 vim.keymap.set("n", "<F2>", function()
     if #(vim.opt.colorcolumn:get()) == 0 then
-        vim.opt.colorcolumn = "80"
+        vim.opt.colorcolumn = {"80"}
     else
-        vim.opt.colorcolumn = ""
+        vim.opt.colorcolumn = {}
     end
 end)
 
@@ -92,10 +92,106 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     "EdenEast/nightfox.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     "mhinz/vim-signify",
+    "ibhagwan/fzf-lua",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
 })
 
 -- Plugins related config
 vim.cmd("colorscheme nightfox")
+
 -- Allows vim-signify to update in realtime
 vim.opt.updatetime = 100 
+
+-- FZF
+local fzf = require("fzf-lua")
+vim.keymap.set("n", "<Leader>f", fzf.files)
+vim.keymap.set("n", "<Leader>s", fzf.git_status)
+vim.keymap.set("n", "<Leader>g", fzf.live_grep)
+vim.keymap.set("v", "<Leader>v", fzf.grep_visual)
+
+-- TreeSitter
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "rust"},
+  highlight = {enable = true},
+}
+
+
+-- LSP
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+servers = {"pylsp"}
+for _, server in ipairs(servers) do
+    lspconfig[server].setup({capabilities = capabilites})
+end
+
+vim.keymap.set("n", ",e", vim.diagnostic.open_float)
+vim.keymap.set("n", ",n", vim.diagnostic.goto_next)
+vim.keymap.set("n", ",p", vim.diagnostic.goto_prev)
+vim.keymap.set("n", ",q", vim.diagnostic.setloclist)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', ',r', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', ',k', vim.lsp.buf.hover, opts)
+    end
+})
+
+
+-- Autocompletion
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+        ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+        -- C-b (back) C-f (forward) for snippet placeholder navigation.
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        else
+            fallback()
+        end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+})
