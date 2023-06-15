2d768cd72c612e0278fd481e91f8ffaf4ef4af09
Vicente Bartual <vicentebartualf@gmail.com>
2023-06-15 02:36:50 +0200

    Update nvim config to lua

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,101 @@
+vim.opt.encoding = "utf-8"
+vim.opt.mouse = "a"
+vim.g.mapleader = " "
+vim.opt.hlsearch = true
+vim.opt.number = true
+vim.opt.cursorline = true
+
+-- Clipboard copy/paste
+vim.keymap.set("n", "<Leader>d", "\"+d")
+vim.keymap.set("n", "<Leader>y", "\"+y")
+vim.keymap.set("n", "<Leader>p", "\"+p")
+vim.keymap.set("v", "<Leader>d", "\"+d")
+vim.keymap.set("v", "<Leader>y", "\"+y")
+vim.keymap.set("v", "<Leader>p", "\"+p")
+
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true -- Shift indent to shiftwidth multiple
+vim.keymap.set("i", "<S-Tab>", "<C-d>")
+
+-- Navigation
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("n", "<C-e>", "3<C-e>")
+vim.keymap.set("n", "<C-y>", "3<C-y>")
+
+-- Status line
+vim.cmd([[
+" Status line
+autocmd WinLeave * highlight StatusModeColor ctermfg=15 ctermbg=239 cterm=Bold
+function! StatusMode()
+    let mode = mode()
+    if mode == "n"
+        return " NORMAL "
+    elseif mode == "i"
+        return " INSERT "
+    elseif mode == "v"
+        return " VISUAL "
+    else
+        return ""
+    endif
+endfunction
+set laststatus=2
+set statusline=
+set statusline+=\ %{StatusMode()}
+set statusline+=\ %f%m%=
+set statusline+=%{v:register}
+set statusline+=\ %y
+set statusline+=\ %{&fileencoding?&fileencoding:&encoding}
+set statusline+=\ %3p%%\ (%l/%L)\ %4l:%-3c
+]])
+
+
+-- Functions
+-- F2: Set 80 char limit
+vim.keymap.set("n", "<F2>", function()
+    if #(vim.opt.colorcolumn:get()) == 0 then
+        vim.opt.colorcolumn = "80"
+    else
+        vim.opt.colorcolumn = ""
+    end
+end)
+
+-- F3: Show whitespaces
+vim.opt.listchars = {space = "·", eol = '↲', tab = '▸ ', trail = '~',
+                     precedes = "«", extends = "»"}
+vim.keymap.set("n", "<F3>", ":set list!<CR>", {silent = true})
+
+
+
+-- Lazy nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "EdenEast/nightfox.nvim",
+    "mhinz/vim-signify",
+})
+
+-- Plugins related config
+vim.cmd("colorscheme nightfox")
+-- Allows vim-signify to update in realtime
+vim.opt.updatetime = 100 
