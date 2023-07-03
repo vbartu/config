c109d22f59fd15680f5c772c3d835078ebaf51fb
Vicente Bartual <vicentebartualf@gmail.com>
2023-07-03 11:33:56 +0200

    Add parameters to autossh

--- scripts/autossh.sh
+++ scripts/autossh.sh
@@ -1,11 +1,23 @@
- #!/bin/bash
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "ERROR: Missing host parameter (user@host)"
+    exit 1
+fi
+
+HOST=$1
+PORT=43043
+
+if [[ -n $2 ]]; then
+    PORT=$2
+fi
 
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa
 
  while true; do
     echo "try to connect..."
-    ssh -N -o ServerAliveInterval=240 -R 43024:localhost:22 user@192.168.0.100
+    ssh -N -o ServerAliveInterval=240 -R $PORT:localhost:22 $HOST
     echo "restarting in 5 seconds.."
     sleep 5
  done
