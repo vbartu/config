b7881ce34d58b768b9440dcc756eb12a9d96048e
Vicente Bartual <vicentebartualf@gmail.com>
2024-06-07 08:30:36 -0700

    Remove Comment.nvim (now builtin!!)

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -137,8 +137,6 @@ require("lazy").setup({
     "neovim/nvim-lspconfig",
     "Mofiqul/trld.nvim",
     "nmac427/guess-indent.nvim",
-    "numToStr/Comment.nvim",
-    lazy = false,
     -- Completion
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-buffer",
@@ -150,7 +148,6 @@ require("lazy").setup({
 -- Plugins related config
 vim.cmd("colorscheme nightfox")
 require("guess-indent").setup({})
-require("Comment").setup()
 
 -- Allows vim-signify to update in realtime
 vim.opt.updatetime = 100 
