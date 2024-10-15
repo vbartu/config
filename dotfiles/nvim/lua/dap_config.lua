2e7e27648ab3e1864206ffda8f15658bda47f3d5
Vicente Bartual <vicentebartualf@gmail.com>
2024-10-15 09:29:05 +0000

    Add gdb dap config

--- dotfiles/nvim/lua/dap_config.lua
+++ dotfiles/nvim/lua/dap_config.lua
@@ -1,6 +1,15 @@
 local dap = require("dap")
 local widgets = require("dap.ui.widgets")
 
+dap.defaults.fallback.terminal_win_cmd = 'tabnew'
+
+-- dap.defaults.fallback.external_terminal = {
+--   command = '/usr/bin/alacritty';
+--   args = {'-e'};
+-- }
+-- dap.defaults.fallback.force_external_terminal = true
+
+
 local hover_widget = nil
 function toggle_hover()
   if hover_widget == nil then
@@ -11,6 +20,9 @@ function toggle_hover()
   end
 end
 
+function step_over_i () dap.step_over({steppingGranularity="instruction"}) end
+function step_into_i () dap.step_into({steppingGranularity="instruction"}) end
+
 vim.keymap.set("n", "mb", dap.toggle_breakpoint)
 vim.keymap.set("n", "ml", dap.list_breakpoints)
 vim.keymap.set("n", "mo", dap.clear_breakpoints)
@@ -19,6 +31,8 @@ vim.keymap.set("n", "mp", dap.pause)
 vim.keymap.set("n", "mt", dap.close)
 vim.keymap.set("n", "mn", dap.step_over)
 vim.keymap.set("n", "ms", dap.step_into)
+vim.keymap.set("n", "mni", step_over_i )
+vim.keymap.set("n", "msi", step_into_i )
 vim.keymap.set("n", "mf", dap.step_out)
 vim.keymap.set("n", "mz", dap.run_to_cursor)
 vim.keymap.set("n", "mr", dap.repl.toggle)
@@ -62,3 +76,37 @@ dap.configurations.python = {
     program = "${file}";
   },
 }
+
+-- C/C++
+dap.adapters.cppdbg = {
+  id = "cppdbg",
+  type = "executable",
+  command = "OpenDebugAD7",
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
