c7ccba1e03584a836c373267ea9660dfa43bba96
Vicente Bartual <vicentebartualf@gmail.com>
2026-04-24 04:21:29 -0700

    Enable tree-sitter indent

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -181,6 +181,7 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "rust"},
   highlight = {enable = true},
+  indent = {enable=true},
 }
 
 -- LSP
